edc96360d8008812db8131b461eee68c449c5e3c
我有点帅哦 <1543995180@qq.com>
2025-02-14 17:28:31 +0800

    update for win2022

--- UnattendResources/CustomResources/RunAfterWindowsUpdates.ps1
+++ UnattendResources/CustomResources/RunAfterWindowsUpdates.ps1
@@ -1,1 +1,24 @@
-echo "RunAfterWindowsUpdates.ps1============"
+function Write-HostLog {
+    <#
+    .SYNOPSIS
+     Uses KVP to communicate to the Hyper-V host the status of the various stages
+     of the imaging generation. This feature works only if the VM where this script
+     runs is spawned on Hyper-V and the 'Data Exchange' (aka Key Value Pair Exchange)
+     is enabled for the instance. On KVM / ESXi / baremetal, this method is NOOP.
+    #>
+    Param($Stage = "Default",
+          $StageLog
+    )
+
+    $KVPOutgoingRegistryKey = "HKLM://SOFTWARE/Microsoft/Virtual Machine/Auto"
+    if ($Stage -and $StageLog -and (Test-Path $KVPOutgoingRegistryKey)) {
+        Set-ItemProperty $KVPOutgoingRegistryKey -Name "ImageGenerationLog-${Stage}" `
+            -Value $StageLog -ErrorAction SilentlyContinue
+    }
+}
+
+Write-HostLog ">>>>>" "RunAfterWindowsUpdates.ps1============start!"
+
+
+Write-HostLog "<<<" "RunAfterWindowsUpdates.ps1===============finish!"
+
--- UnattendResources/CustomResources/RunBeforeWindowsUpdates.ps1
+++ UnattendResources/CustomResources/RunBeforeWindowsUpdates.ps1
@@ -1,8 +1,31 @@
+function Write-HostLog {
+    <#
+    .SYNOPSIS
+     Uses KVP to communicate to the Hyper-V host the status of the various stages
+     of the imaging generation. This feature works only if the VM where this script
+     runs is spawned on Hyper-V and the 'Data Exchange' (aka Key Value Pair Exchange)
+     is enabled for the instance. On KVM / ESXi / baremetal, this method is NOOP.
+    #>
+    Param($Stage = "Default",
+          $StageLog
+    )
+
+    $KVPOutgoingRegistryKey = "HKLM://SOFTWARE/Microsoft/Virtual Machine/Auto"
+    if ($Stage -and $StageLog -and (Test-Path $KVPOutgoingRegistryKey)) {
+        Set-ItemProperty $KVPOutgoingRegistryKey -Name "ImageGenerationLog-${Stage}" `
+            -Value $StageLog -ErrorAction SilentlyContinue
+    }
+}
+
+Write-HostLog ">>>>>" "RunBeforeWindowsUpdates.ps1============start!"
+
 echo "RunBeforeWindowsUpdates.ps1============"
 # 获取网卡的接口索引 ================== 第2次重启（不需要重启）
 $interfaceIndex = (Get-NetAdapter | Where-Object { $_.Name -eq "以太网" }).ifIndex
 # 配置静态 IP 地址
-New-NetIPAddress -InterfaceIndex $interfaceIndex -IPAddress 172.28.188.177 -PrefixLength 24 -DefaultGateway 172.28.188.1
+New-NetIPAddress -InterfaceIndex $interfaceIndex -IPAddress 172.28.188.188 -PrefixLength 24 -DefaultGateway 172.28.188.1
 # 配置 DNS 服务器
 Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses 8.8.8.8, 8.8.4.4
 #Restart-Computer
+
+Write-HostLog "<<<" "RunBeforeWindowsUpdates.ps1============finish!"
